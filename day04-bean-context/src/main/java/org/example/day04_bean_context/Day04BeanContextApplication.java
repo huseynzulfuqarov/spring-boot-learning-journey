e12edddd49cee96566ee3c5146b8aa35390612d7
Huseyn Zulfugarov <130462640+huseynzulfuqarov@users.noreply.github.com>
2025-07-31 21:30:40 +0400

    Initial commit

---
+++ day04-bean-context/src/main/java/org/example/day04_bean_context/Day04BeanContextApplication.java
@@ -0,0 +1,13 @@
+package org.example.day04_bean_context;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day04BeanContextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day04BeanContextApplication.class, args);
+	}
+
+}
---
+++ day04-bean-context/src/test/java/org/example/day04_bean_context/Day04BeanContextApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.day04_bean_context;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day04BeanContextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
