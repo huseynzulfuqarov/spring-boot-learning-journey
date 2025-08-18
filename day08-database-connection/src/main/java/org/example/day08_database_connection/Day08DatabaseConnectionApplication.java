95c514828a77a171b56d8c62f606005a1f77c82b
Huseyn Zulfugarov <130462640+huseynzulfuqarov@users.noreply.github.com>
2025-08-18 22:40:41 +0400

    Initial commit

---
+++ day08-database-connection/src/main/java/org/example/day08_database_connection/Day08DatabaseConnectionApplication.java
@@ -0,0 +1,13 @@
+package org.example.day08_database_connection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day08DatabaseConnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day08DatabaseConnectionApplication.class, args);
+	}
+
+}
---
+++ day08-database-connection/src/test/java/org/example/day08_database_connection/Day08DatabaseConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.day08_database_connection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day08DatabaseConnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
