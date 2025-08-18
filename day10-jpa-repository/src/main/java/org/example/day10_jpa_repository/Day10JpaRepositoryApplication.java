9f9b3c8b97d3f366d08ad19609532280b1c5b5f9
Huseyn Zulfugarov <130462640+huseynzulfuqarov@users.noreply.github.com>
2025-08-19 00:50:57 +0400

    Initial commit

---
+++ day10-jpa-repository/src/main/java/org/example/day10_jpa_repository/Day10JpaRepositoryApplication.java
@@ -0,0 +1,13 @@
+package org.example.day10_jpa_repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day10JpaRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day10JpaRepositoryApplication.class, args);
+	}
+
+}
---
+++ day10-jpa-repository/src/test/java/org/example/day10_jpa_repository/Day10JpaRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.day10_jpa_repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day10JpaRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
