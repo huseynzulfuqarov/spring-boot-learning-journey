7e9b1ea47e574208548435c9dff97f5853c6935c
Huseyn Zulfugarov <130462640+huseynzulfuqarov@users.noreply.github.com>
2025-08-19 23:35:24 +0400

    Core logic of controller

--- day12-product-inventory/src/main/java/org/example/day12_product_inventory/controller/ProductController.java
+++ day12-product-inventory/src/main/java/org/example/day12_product_inventory/controller/ProductController.java
@@ -1,7 +1,44 @@
 package org.example.day12_product_inventory.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.example.day12_product_inventory.entity.Product;
+import org.example.day12_product_inventory.repository.ProductRepository;
+import org.example.day12_product_inventory.service.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
+@RequestMapping("/products")
 public class ProductController {
+    private final ProductService productService;
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> findAll() {
+        return  productService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product findById(@PathVariable Long id) {
+        return productService.findById(id);
+    }
+
+    @PostMapping()
+    public Product save(@RequestBody Product product) {
+        return productService.save(product);
+    }
+
+    @DeleteMapping("")
+    public void delete(@RequestBody Product product) {
+        productService.deleteById(product.getId());
+    }
+
+    @PatchMapping("/{id}")
+    public Product update(@PathVariable Long id, @RequestBody Map<String, Object> updates) {
+        return productService.updateProductPartially(id, updates);
+    }
 }
