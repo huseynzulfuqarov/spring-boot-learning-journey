be9206181073682a95febd8f370b2c386270aa36
Huseyn Zulfugarov <130462640+huseynzulfuqarov@users.noreply.github.com>
2025-08-19 22:56:11 +0400

    Initial set up

---
+++ day12-product-inventory/src/main/java/org/example/day12_product_inventory/Day12ProductInventoryApplication.java
@@ -0,0 +1,13 @@
+package org.example.day12_product_inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day12ProductInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day12ProductInventoryApplication.class, args);
+	}
+
+}
---
+++ day12-product-inventory/src/test/java/org/example/day12_product_inventory/Day12ProductInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.day12_product_inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day12ProductInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
