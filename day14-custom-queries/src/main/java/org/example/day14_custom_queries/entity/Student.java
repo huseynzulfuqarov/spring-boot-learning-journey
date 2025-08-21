c28faffb6aeba7373b0e0eed525bc81b7c4a2e0e
Huseyn Zulfugarov <130462640+huseynzulfuqarov@users.noreply.github.com>
2025-08-21 23:53:08 +0400

    Student class completed

--- day14-custom-queries/src/main/java/org/example/day14_custom_queries/entity/Student.java
+++ day14-custom-queries/src/main/java/org/example/day14_custom_queries/entity/Student.java
@@ -1,4 +1,35 @@
 package org.example.day14_custom_queries.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "Student_tb")
 public class Student {
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private int age;
+    @Column(unique = true, nullable = false) //@Id???
+    private String email;
+    @Column(length = 500)
+    private String address;
+
+    public Student() {
+    }
+
+    public Student(Long id, String name, int age, String email) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
 }
