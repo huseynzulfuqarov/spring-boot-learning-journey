b871d4dadf4cbcfa81d75b02309b623c15da1850
Huseyn Zulfugarov <130462640+huseynzulfuqarov@users.noreply.github.com>
2025-08-21 23:12:10 +0400

    implement full crud and derived queries for student repository

---
+++ day13-derived-queries/src/test/java/com/example/day13_derived_queries/Day13DerivedQueriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day13_derived_queries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day13DerivedQueriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
