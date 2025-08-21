9957088ef94fd3eafcb9d2eda7c7753dc4da0d40
Huseyn Zulfugarov <130462640+huseynzulfuqarov@users.noreply.github.com>
2025-08-21 23:48:00 +0400

    Initial commit

---
+++ day14-custom-queries/src/main/java/org/example/day14_custom_queries/Day14CustomQueriesApplication.java
@@ -0,0 +1,13 @@
+package org.example.day14_custom_queries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day14CustomQueriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day14CustomQueriesApplication.class, args);
+	}
+
+}
---
+++ day14-custom-queries/src/test/java/org/example/day14_custom_queries/Day14CustomQueriesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.day14_custom_queries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day14CustomQueriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
