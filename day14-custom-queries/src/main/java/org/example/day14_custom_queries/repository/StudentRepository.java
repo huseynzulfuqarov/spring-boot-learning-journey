29bae3e9f50cde468e7f4a9abd2c356fcae63a26
Huseyn Zulfugarov <130462640+huseynzulfuqarov@users.noreply.github.com>
2025-08-22 00:24:09 +0400

    add custom jpql and native sql queries

--- day14-custom-queries/src/main/java/org/example/day14_custom_queries/repository/StudentRepository.java
+++ day14-custom-queries/src/main/java/org/example/day14_custom_queries/repository/StudentRepository.java
@@ -1,4 +1,22 @@
 package org.example.day14_custom_queries.repository;
 
-public class StudentRepository {
+import org.example.day14_custom_queries.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    @Query("SELECT s FROM Student s WHERE s.email NOT LIKE %:domain")
+    List<Student> findStudentsWithEmailNotEndingWith(@Param("domain") String domain);
+
+    @Query("SELECT s FROM Student s WHERE s.age BETWEEN :minAge AND :maxAge")
+    List<Student> findStudentsInAgeRange(@Param("minAge") int min, @Param("maxAge") int max);
+
+    @Query(value = "SELECT * FROM Student_tb WHERE name = :studentName LIMIT 1", nativeQuery = true)
+    Optional<Student> findStudentByNameNative(@Param("studentName") String name);
 }
